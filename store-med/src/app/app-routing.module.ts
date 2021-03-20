852574bbbd8f8af1f6c9d6e4f036c27418c8c977
SeJun Bae <sejun.bae@outlook.com>
2021-03-19 20:51:54 -0400

    Started sign up page.

--- store-med/src/app/app-routing.module.ts
+++ store-med/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 const routes: Routes = [
+  { path: 'sign-up', component: HomepageComponent },
   { path: '', component: HomepageComponent, pathMatch: 'full' },
 ];
 
--- store-med/src/app/app.module.ts
+++ store-med/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
+    SignupComponent,
 
   ],
   imports: [
