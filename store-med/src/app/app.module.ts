14b20dbd3828ace9784c87951a6815093066b157
SeJun Bae <sejun.bae@outlook.com>
2021-03-19 22:05:09 -0400

    Added Poppins font.

--- store-med/src/app/app.module.ts
+++ store-med/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [AppComponent, HomepageComponent, SignupComponent],
+  declarations: [AppComponent, HomepageComponent, SignupComponent, LoginComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
