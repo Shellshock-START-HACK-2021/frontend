207f90901fcc8c4a9c3dad91e7e63c4b7ea88a4f
SeJun Bae <sejun.bae@outlook.com>
2021-03-20 05:07:12 -0400

    Minor changes again

--- store-med/src/app/app-routing.module.ts
+++ store-med/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoadingComponent } from './dashboard/loading/loading.component';
 
 
 const routes: Routes = [
+  { path: 'dashboard/Loading', component: LoadingComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'login', component: LoginComponent },
   { path: 'sign-up', component: SignupComponent },
