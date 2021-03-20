656436552cc4c987f5deac03d0d912349c9ca8a0
SeJun Bae <sejun.bae@outlook.com>
2021-03-20 01:12:57 -0400

    Integrated frontend and backend for login and signup. Getting CORS error on post request. Modified Login and registration to look like the design.

--- store-med/src/app/login/login.component.ts
+++ store-med/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  myForm: FormGroup;
+  backendUrl = 'https://storemed-dumb.herokuapp.com/auth/login';
 
-  ngOnInit(): void {}
+  constructor(
+    private router: Router,
+
+    private fb: FormBuilder,
+
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      email: '',
+      password: '',
+    });
+  }
+
+  get email() {
+    return this.myForm.get('email');
+  }
+
+  get password() {
+    return this.myForm.get('password');
+  }
+
+  onSubmit() {
+    const header = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const body = this.myForm.value;
+    this.http.post(this.backendUrl, body, { headers: header }).subscribe(
+      (data) => {
+        console.log('Sucess');
+        console.log(data);
+      },
+      (error) => {
+        console.log('error');
+        console.log(error);
+      }
+    );
+  }
 }
