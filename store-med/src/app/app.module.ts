2e8d11a134908b3a2431c667ab93ddf8afa279d1
SeJun Bae <sejun.bae@outlook.com>
2021-03-19 21:57:46 -0400

    Finished registration page

--- store-med/src/app/app.module.ts
+++ store-med/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -8,17 +8,9 @@ import { SignupComponent } from './signup/signup.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomepageComponent,
-    SignupComponent,
-
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomepageComponent, SignupComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
